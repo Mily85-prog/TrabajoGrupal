615ca1b8f4ef7c4855fea65a64c24364694cc8a9
Snyder <snayderjosehornas06@gmail.com>
2025-09-27 18:21:27 -0500

    Practica de cherry pick

--- trabajoGrupalSemana03/src/trabajogrupalsemana03/TrabajoGrupalSemana03.java
+++ trabajoGrupalSemana03/src/trabajogrupalsemana03/TrabajoGrupalSemana03.java
@@ -17,14 +17,9 @@ public class TrabajoGrupalSemana03 {
         System.out.println("Aprendiendo Ramas");
         System.out.println("Bienvenido al proyecto con Git y GitHub");
         System.out.println("Hola mundo soy Milagros");
-        // TODO code application logic here
-        public class TrabajoGrupalSemana03 {
-    public static void main(String[] args) {
         System.out.println("Bienvenido al Trabajo Grupal - Semana 03");
-        
-        // Crear instancia de la clase Trabajo
-        Trabajo trabajo = new Trabajo();
-        trabajo.mostrarInfo();
+        System.out.println("HOLA SPY SNYDER BUENAS PRACTICAS");
+        System.out.println("ESTA ES MI EVIDENCIA");
     }
 }
     }
