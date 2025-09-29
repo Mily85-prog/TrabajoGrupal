26422577433bcd818301f0bdc594a7620644d042
Jhajayra Mamani <jhajayrabelen15@gmail.com>
2025-09-28 23:50:00 -0500

    Cambio2

---
+++ trabajoGrupalSemana03/src/trabajogrupalsemana03/TrabajoGrupalSemana03.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajogrupalsemana03;
+
+/**
+ *
+ * @author Mily
+ */
+public class TrabajoGrupalSemana03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aprendiendo Ramas");
+        System.out.println("Bienvenido al proyecto con Git y GitHub");
+        System.out.println("Hola mundo soy Milagros");
+        rama-milagros
+        System.out.println("Prueba de git stash");
+        System.out.println("Prueba para revert");
+
+
+        // TODO code application logic here
+  revert-4-feature/mejores-practicas
+=======
+       rama-milagros
+
+        System.out.println("Bienvenido al Trabajo Grupal - Semana 03");
+        System.out.println("HOLA SPY SNYDER BUENAS PRACTICAS");
+        System.out.println("ESTA ES MI EVIDENCIA");
+    
+=======
+       ramaBelen 
+       System.out.println("Bienvenido-Navegación de ramas-Semana3");
+       System.out.println("Holi, soy Belen ");
+       System.out.println("Evidencio mi practica ");
+    }
+
+}
+        practica-sobrecarga
+
+        public class TrabajoGrupalSemana03 {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Trabajo Grupal - Semana 03");
+        
+        // Crear instancia de la clase Trabajo
+        Trabajo trabajo = new Trabajo();
+        trabajo.mostrarInfo();
+    }
+}
+ feature/mensaje
+ feature/mensaje
+    }
+    
+}
