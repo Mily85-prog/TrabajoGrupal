3be3865d9023de65eec030d9d7f514e0d8b66749
Sofía <quispelucanosofialariza@gmail.com>
2025-09-28 23:12:34 -0500

    elimino más cambios a ramaSofia

--- src/Principal.java
+++ src/Principal.java
@@ -7,6 +7,7 @@ public class Principal
 {
 public static void main(String[] args)
     {
+        
         Controlador controlador = new Controlador(new Scanner (System.in));
         try 
         {
